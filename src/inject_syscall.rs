0cb1455be5a5690ce43a01166f29764cf4c94e1e
Jörg Thalheim <joerg@thalheim.io>
2021-02-22 08:02:16 +0000

    implement inject syscalls

--- src/inject_syscall.rs
+++ src/inject_syscall.rs
@@ -1,18 +1,23 @@
-use libc::{c_int, c_ulong};
-use libc::{c_void, pid_t, user_regs_struct};
+use libc::SYS_ioctl;
+use libc::{c_int, c_long, c_ulong, c_void, pid_t};
+use nix::sys::signal::Signal;
+use nix::sys::wait::{waitpid, WaitStatus};
 use nix::unistd::Pid;
+use simple_error::bail;
 use simple_error::try_with;
 use std::fs;
+use std::mem;
 use std::os::unix::prelude::RawFd;
 use std::path::PathBuf;
 
-use crate::cpu;
+use crate::cpu::{self, Regs};
 use crate::ptrace;
 use crate::result::Result;
 
 pub struct Process {
     process_idx: usize,
-    saved_regs: user_regs_struct,
+    saved_regs: Regs,
+    saved_text: c_long,
     threads: Vec<ptrace::Thread>,
 }
 
@@ -42,148 +47,152 @@ pub fn attach(pid: Pid) -> Result<Process> {
         threads[process_idx].getregs(),
         "cannot get registers for main process"
     );
-    let ip = cpu::get_ip(&saved_regs);
+    let ip = saved_regs.ip();
     let saved_text = try_with!(
         threads[process_idx].read(ip as *mut c_void),
         "cannot get text for main process"
+    );
+    try_with!(
+        threads[process_idx].write(ip as *mut c_void, cpu::SYSCALL_TEXT as *mut c_void),
+        "cannot patch syscall instruction"
     );
 
     Ok(Process {
         process_idx,
         saved_regs,
+        saved_text,
         threads,
     })
 }
 
+macro_rules! syscall_args {
+    ($regs:expr, $nr:expr) => {
+        ($regs).prepare_syscall(&[$nr, 0, 0, 0, 0, 0, 0])
+    };
+
+    ($regs:expr, $nr:expr, $a1:expr) => {
+        ($regs).prepare_syscall(&[$nr, $a1 as c_ulong, 0, 0, 0, 0, 0])
+    };
+
+    ($regs:expr, $nr:expr, $a1:expr, $a2:expr) => {
+        ($regs).prepare_syscall(&[$nr, $a1 as c_ulong, $a2 as c_ulong, 0, 0, 0, 0])
+    };
+
+    ($regs:expr, $nr:expr, $a1:expr, $a2:expr, $a3:expr) => {
+        $regs.prepare_syscall(&[$nr, $a1 as c_ulong, $a2 as c_ulong, $a3 as c_ulong, 0, 0, 0])
+    };
+
+    ($regs:expr, $nr:expr, $a1:expr, $a2:expr, $a3:expr, $a4:expr) => {
+        $regs.prepare_syscall(&[
+            $nr,
+            $a1 as c_ulong,
+            $a2 as c_ulong,
+            $a3 as c_ulong,
+            $a4 as c_ulong,
+            0,
+            0,
+        ])
+    };
+
+    ($regs:expr, $nr:expr, $a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr) => {
+        $regs.prepare_syscall(&[
+            $nr,
+            $a1 as c_ulong,
+            $a2 as c_ulong,
+            $a3 as c_ulong,
+            $a4 as c_ulong,
+            $a5 as c_ulong,
+            0,
+        ])
+    };
+
+    ($regs:expr, $nr:expr, $a1:expr, $a2:expr, $a3:expr, $a4:expr, $a5:expr, $a6:expr) => {
+        $regs.prepare_syscall(&[
+            $nr,
+            $a1 as c_ulong,
+            $a2 as c_ulong,
+            $a3 as c_ulong,
+            $a4 as c_ulong,
+            $a5 as c_ulong,
+            $a6 as c_ulong,
+        ])
+    };
+}
+
 impl Process {
     pub fn ioctl(&self, fd: RawFd, request: c_ulong, arg: c_int) -> Result<c_int> {
-        Ok(0)
+        let args = syscall_args!(
+            self.saved_regs,
+            SYS_ioctl as c_ulong,
+            fd as c_ulong,
+            request,
+            arg
+        );
+
+        return self.syscall(&args).map(|v| v as c_int);
     }
-    //def ioctl(self, fd: int, request: int, arg: Any = 0) -> int:
-    //    return ctypes.c_int(
-    //        self.syscall(SYSCALL_NAMES["ioctl"], fd, request, arg)
-    //    ).value
+
+    fn syscall(&self, regs: &Regs) -> Result<isize> {
+        try_with!(
+            self.main_thread().setregs(regs),
+            "cannot set system call args"
+        );
+        // FIXME: on arm we would need PTRACE_SET_SYSCALL
+        loop {
+            try_with!(self.main_thread().syscall(), "cannot run syscall in thread");
+
+            let mut status = try_with!(waitpid(self.main_thread().tid, None), "waitpid failed");
+
+            // why do we need this one?
+            if let WaitStatus::Stopped(_, Signal::SIGTRAP) = status {
+                try_with!(self.main_thread().syscall(), "cannot run syscall in thread");
+                status = try_with!(waitpid(self.main_thread().tid, None), "waitpid failed");
+            }
+
+            match status {
+                WaitStatus::PtraceEvent(_, _, _) => {
+                    let result_regs =
+                        try_with!(self.main_thread().getregs(), "cannot syscall results");
+                    assert!(
+                        self.saved_regs.ip()
+                            == result_regs.ip() - mem::size_of_val(&cpu::SYSCALL_TEXT) as u64
+                    );
+                    try_with!(
+                        self.main_thread().setregs(&self.saved_regs),
+                        "failed to restore old syscalls"
+                    );
+                    return Ok(result_regs.syscall_ret() as isize);
+                }
+                WaitStatus::PtraceSyscall(_) => {
+                    return bail!("got unexpected ptrace syscall event")
+                }
+                WaitStatus::StillAlive => {
+                    return bail!("got unexpected still-alive waitpid() event")
+                }
+                WaitStatus::Continued(_) => {} // noop
+                WaitStatus::Stopped(_, signal) => {
+                    // should not happen usually, so log it
+                    return bail!("process was stopped by by signal: {}", signal);
+                }
+                WaitStatus::Exited(_, status) => return bail!("process exited with: {}", status),
+                WaitStatus::Signaled(_, signal, coredumped) => {
+                    return bail!("process was stopped by signal: {}", signal)
+                }
+            }
+        }
+    }
+
+    fn main_thread(&self) -> &ptrace::Thread {
+        &self.threads[self.process_idx]
+    }
 }
 
 impl Drop for Process {
     fn drop(&mut self) {
-        let _ = self.threads[self.process_idx].setregs(self.saved_regs);
+        let _ = self.main_thread().write(
+            self.saved_regs.ip() as *mut c_void,
+            self.saved_text as *mut c_void,
+        );
+        let _ = self.main_thread().setregs(&self.saved_regs);
     }
 }
-
-//@contextmanager
-//def attach(pid: int) -> Generator[Process, None, None]:
-//    threads = []
-//
-//    try:
-//        for thread in os.listdir(f"/proc/{pid}/task"):
-//            tid = int(thread)
-//            ptrace.attach(tid)
-//            _, status = os.waitpid(tid, 0)
-//            assert os.WIFSTOPPED(status), "Could attach to pid"
-//            threads.append(tid)
-//
-//        with save_regs(pid) as regs:
-//            with save_text(pid, regs.ip):
-//                ptrace.poketext(pid, regs.ip, SYSCALL_TEXT)
-//                yield Process(pid, regs)
-//    finally:
-//        for tid in threads:
-//            ptrace.detach(tid)
-
-//import ctypes
-//import os
-//import signal
-//from contextlib import contextmanager
-//from typing import Any, Generator
-//
-//from . import cpu, ptrace
-//from .syscalls import SYSCALL_NAMES, SYSCALL_TEXT
-//
-//
-//class SyscallError(OSError):
-//    pass
-//
-//
-//class ExitError(SyscallError):
-//    pass
-//
-//
-//class Process:
-//    def __init__(self, pid: int, saved_regs: cpu.user_regs_struct) -> None:
-//        self.pid = pid
-//        self.saved_regs = saved_regs
-//
-//    def syscall(self, *args: Any) -> int:
-//        regs = self.saved_regs.prepare_syscall(*args)
-//        ptrace.setregs(self.pid, regs)
-//        # FIXME: on arm we would need PTRACE_SET_SYSCALL
-//        ptrace.syscall(self.pid)
-//        _, status = os.waitpid(self.pid, 0)
-//        if os.WIFSTOPPED(status) and os.WEXITSTATUS(status) & ~0x80 == signal.SIGTRAP:
-//            ptrace.syscall(self.pid)
-//            _, status = os.waitpid(self.pid, 0)
-//
-//        if os.WIFSTOPPED(status):
-//            result = ptrace.getregs(self.pid)
-//            assert (
-//                self.saved_regs.ip == result.ip - 2
-//            ), f"{self.saved_regs.ip} != {result.ip - 2}"
-//            ptrace.setregs(self.pid, self.saved_regs)
-//            return result.syscall_result()
-//
-//        if os.WIFEXITED(status):
-//            exit_code = os.WEXITSTATUS(status)
-//            raise ExitError(f"process exited with: {exit_code}")
-//        elif os.WIFSIGNALED(status):
-//            sigcode = os.WTERMSIG(status)
-//            raise ExitError(
-//                f"process stopped by signal: {sigcode} ({signal.strsignal(sigcode)})"
-//            )
-//        else:
-//            raise SyscallError("failed to invoke syscall")
-//
-//    def ioctl(self, fd: int, request: int, arg: Any = 0) -> int:
-//        return ctypes.c_int(
-//            self.syscall(SYSCALL_NAMES["ioctl"], fd, request, arg)
-//        ).value
-//
-//
-//@contextmanager
-//def save_regs(pid: int) -> Generator[cpu.user_regs_struct, None, None]:
-//    old_regs = ptrace.getregs(pid)
-//    assert old_regs.ip != 0
-//    try:
-//        yield old_regs
-//    finally:
-//        ptrace.setregs(pid, old_regs)
-//
-//
-//@contextmanager
-//def save_text(pid: int, ip: int) -> Generator[int, None, None]:
-//    old_text = ptrace.peektext(pid, ip)
-//    try:
-//        yield old_text
-//    finally:
-//        ptrace.poketext(pid, ip, old_text)
-//
-//
-//@contextmanager
-//def attach(pid: int) -> Generator[Process, None, None]:
-//    threads = []
-//
-//    try:
-//        for thread in os.listdir(f"/proc/{pid}/task"):
-//            tid = int(thread)
-//            ptrace.attach(tid)
-//            _, status = os.waitpid(tid, 0)
-//            assert os.WIFSTOPPED(status), "Could attach to pid"
-//            threads.append(tid)
-//
-//        with save_regs(pid) as regs:
-//            with save_text(pid, regs.ip):
-//                ptrace.poketext(pid, regs.ip, SYSCALL_TEXT)
-//                yield Process(pid, regs)
-//    finally:
-//        for tid in threads:
-//            ptrace.detach(tid)
--- src/ptrace.rs
+++ src/ptrace.rs
@@ -1,24 +1,64 @@
-use crate::result::Result;
-use libc::{c_long, c_void, user_regs_struct};
-use nix::sys::ptrace::{self, AddressType};
+use libc::{c_long, c_void};
+use nix::errno::Errno;
+use nix::sys::ptrace::{self, AddressType, Request, RequestType};
 use nix::unistd::Pid;
 use simple_error::try_with;
+use std::{mem, ptr};
+
+use crate::cpu::Regs;
+use crate::result::Result;
 
 pub struct Thread {
-    tid: Pid,
+    pub tid: Pid,
+}
+
+/// Get user registers, as with `ptrace(PTRACE_GETREGS, ...)`
+fn getregs(pid: Pid) -> nix::Result<Regs> {
+    ptrace_get_data::<Regs>(Request::PTRACE_GETREGS, pid)
+}
+
+/// Set user registers, as with `ptrace(PTRACE_SETREGS, ...)`
+fn setregs(pid: Pid, regs: &Regs) -> nix::Result<()> {
+    let res = unsafe {
+        libc::ptrace(
+            Request::PTRACE_SETREGS as RequestType,
+            libc::pid_t::from(pid),
+            ptr::null_mut::<c_void>(),
+            &regs as *const _ as *const c_void,
+        )
+    };
+    Errno::result(res).map(drop)
+}
+
+/// Function for ptrace requests that return values from the data field.
+/// Some ptrace get requests populate structs or larger elements than `c_long`
+/// and therefore use the data field to return values. This function handles these
+/// requests.
+fn ptrace_get_data<T>(request: Request, pid: Pid) -> nix::Result<T> {
+    let mut data = mem::MaybeUninit::uninit();
+    let res = unsafe {
+        libc::ptrace(
+            request as RequestType,
+            libc::pid_t::from(pid),
+            ptr::null_mut::<T>(),
+            data.as_mut_ptr() as *const _ as *const c_void,
+        )
+    };
+    Errno::result(res)?;
+    Ok(unsafe { data.assume_init() })
 }
 
 impl Thread {
-    pub fn setregs(&self, regs: user_regs_struct) -> Result<()> {
+    pub fn setregs(&self, regs: &Regs) -> Result<()> {
         Ok(try_with!(
-            ptrace::setregs(self.tid, regs),
+            setregs(self.tid, regs),
             "cannot set registers with ptrace"
         ))
     }
 
-    pub fn getregs(&self) -> Result<user_regs_struct> {
+    pub fn getregs(&self) -> Result<Regs> {
         Ok(try_with!(
-            ptrace::getregs(self.tid),
+            getregs(self.tid),
             "cannot get registers with ptrace"
         ))
     }
