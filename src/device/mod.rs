10423573042a4d72a2152ca42ae908932cbcd627
Pogobanane <aenderboy@gmx.de>
2021-02-26 14:32:11 +0000

    add all missing sub objects

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -3,13 +3,15 @@ mod virtio;
 
 use vm_virtio::device::status::RESET;
 use vm_device::resources::ResourceConstraint;
+use vm_device::device_manager::IoManager;
+use vm_device::bus::{MmioAddress, MmioRange};
 use vm_memory::{GuestMemoryMmap, GuestRegionMmap, FileOffset};
 use vm_memory::guest_memory::GuestAddress;
 use vm_memory::mmap::MmapRegion;
+use event_manager::{EventManager, MutEventSubscriber};
 use std::sync::{Arc, Mutex};
 use simple_error::try_with;
 use std::path::PathBuf;
-use vm_device::bus::{MmioAddress, MmioRange};
 
 use crate::kvm::Hypervisor;
 use crate::device::virtio::block::{self, BlockArgs};
@@ -40,8 +42,8 @@ fn convert(mappings: &Vec<Mapping>) -> GuestMemoryMmap {
             /* TODO check correctness
              * addr_a: *mut c_void = mmap(...);
              * addr = addr_a as *mut u8;
-             * mapping.start: u64
-             * ???
+             * addr is a pointer to a void, so it doesn't matter what rust thinks it points to
+             * mapping.start: u64 
              */
             addr: mapping.start as *mut u8,
             size: (mapping.end - mapping.start) as usize,
@@ -76,13 +78,18 @@ impl Device {
         let range = MmioRange::new(MmioAddress(MMIO_MEM_START), 0x1000).unwrap();
         let mmio_cfg = MmioConfig { range, gsi: 5 };
 
-        let mut guard = None;
+        // TODO is there more we have to do with this mgr?
+        let device_manager = Arc::new(Mutex::new(IoManager::new())); 
+        let mut guard = device_manager.lock().unwrap();
+
+        let mut event_manager = EventManager::<Arc<Mutex<dyn MutEventSubscriber + Send>>>::new().expect("cannot create event manager");
+        // TODO add subscriber (wrapped_exit_handler) and stuff?
 
         let common = CommonArgs {
             mem,
             vmm,
-            event_mgr: &mut self.event_mgr, // TODO
-            mmio_mgr: guard.deref_mut(), // TODO
+            event_mgr: &mut event_manager,
+            mmio_mgr: guard,
             mmio_cfg,
         };
 
