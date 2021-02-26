e1db04e97869ae603904cd3bcc1f9611b74884d0
Pogobanane <aenderboy@gmx.de>
2021-02-26 13:46:01 +0000

    convert Mapping to GuestMemoryMap

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -3,7 +3,9 @@ mod virtio;
 
 use vm_virtio::device::status::RESET;
 use vm_device::resources::ResourceConstraint;
-use vm_memory::GuestMemoryMmap;
+use vm_memory::{GuestMemoryMmap, GuestRegionMmap, FileOffset};
+use vm_memory::guest_memory::GuestAddress;
+use vm_memory::mmap::MmapRegion;
 use std::sync::{Arc, Mutex};
 use simple_error::try_with;
 use std::path::PathBuf;
@@ -12,6 +14,7 @@ use vm_device::bus::{MmioAddress, MmioRange};
 use crate::kvm::Hypervisor;
 use crate::device::virtio::block::{self, BlockArgs};
 use crate::device::virtio::{CommonArgs, MmioConfig};
+use crate::proc::Mapping;
 
 // Where BIOS/VGA magic would live on a real PC.
 const EBDA_START: u64 = 0x9fc00;
@@ -22,6 +25,44 @@ pub const MMIO_MEM_START: u64 = FIRST_ADDR_PAST_32BITS - MEM_32BIT_GAP_SIZE;
 
 type Block = block::Block<Arc<GuestMemoryMmap>>;
 
+fn convert(mappings: &Vec<Mapping>) -> GuestMemoryMmap {
+    let regions: Vec<Arc<GuestRegionMmap>> = vec!{};
+
+    for mapping in mappings {
+        let file = Arc::new(std::fs::File::open(&mapping.pathname).expect("could not open file")); // TODO formatted
+
+        let file_offset = FileOffset {
+            file,
+            start: mapping.offset,
+        };
+
+        let mmap_region = MmapRegion {
+            /* TODO check correctness
+             * addr_a: *mut c_void = mmap(...);
+             * addr = addr_a as *mut u8;
+             * mapping.start: u64
+             * ???
+             */
+            addr: mapping.start as *mut u8,
+            size: (mapping.end - mapping.start) as usize,
+            file_offset: Some(file_offset), // is not actually optional
+            prot: mapping.prot_flags.bits(),
+            flags: mapping.map_flags.bits(),
+            owned: false,
+            hugetlbfs: None,
+        };
+
+        let guest_region_mmap = GuestRegionMmap {
+            mapping: mmap_region,
+            guest_base: GuestAddress(mapping.phys_addr),
+        };
+
+        regions.push(Arc::new(guest_region_mmap));
+    }
+
+    GuestMemoryMmap { regions }
+}
+
 pub struct Device { 
     vmm: Arc<Hypervisor>,
     blkdev: Arc<Mutex<Block>>,
@@ -30,7 +71,7 @@ pub struct Device {
 impl Device {
     pub fn new(vmm: Arc<Hypervisor>) -> Device {
 
-        let mem: Arc<GuestMemoryMmap> = Arc::new(vmm.mappings); // TODO plug GuestMemoryMmap and Mapping together
+        let mem: Arc<GuestMemoryMmap> = Arc::new(convert(&vmm.mappings));
 
         let range = MmioRange::new(MmioAddress(MMIO_MEM_START), 0x1000).unwrap();
         let mmio_cfg = MmioConfig { range, gsi: 5 };
