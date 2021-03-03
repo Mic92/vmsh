40cb8ca09a27e9e3814cbaf4ec9f69a0365c7e76
Jörg Thalheim <joerg@thalheim.io>
2021-03-03 15:29:13 +0000

    satisfy black

--- tests/test_coredump.py
+++ tests/test_coredump.py
@@ -218,7 +218,7 @@ def run_vmsh_command(args: List[str]) -> None:
             "--addamb=cap_sys_ptrace",
             "--",
             "-c",
-            cargoCmd
+            cargoCmd,
         ]
         print("$ " + " ".join(map(quote, cmd)))
         subprocess.run(cmd, check=True)
