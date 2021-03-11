029d41f3c29ffc0fc99ad4e7832e2af066d50841
Jörg Thalheim <joerg@thalheim.io>
2021-03-11 13:37:37 +0000

    validate coredump file

--- tests/test_coredump.py
+++ tests/test_coredump.py
@@ -1,6 +1,11 @@
 import conftest
+import os
+import subprocess
+from tempfile import TemporaryDirectory
 
 
 def test_coredump(helpers: conftest.Helpers) -> None:
-    with helpers.spawn_qemu(helpers.notos_image()) as vm:
-        helpers.run_vmsh_command(["coredump", str(vm.pid)])
+    with TemporaryDirectory() as temp, helpers.spawn_qemu(helpers.notos_image()) as vm:
+        core = os.path.join(temp, "core")
+        helpers.run_vmsh_command(["coredump", str(vm.pid), core])
+        subprocess.run(["readelf", "-a", core], check=True)
