2d852548d4e90ba0427cbbe0b133f0aa193a3e36
Jörg Thalheim <joerg@thalheim.io>
2021-03-26 15:46:46 +0000

    coredump: add mmap method

--- tests/coredump.py
+++ tests/coredump.py
@@ -2,8 +2,11 @@
 import ctypes as ct
 from typing import IO, List
 
+import mmap
+import resource
 from elftools.elf.elffile import ELFFile
-from elftools.elf.segments import NoteSegment
+from elftools.elf.segments import NoteSegment, Segment
+
 from coredump_structs import (
     elf_prstatus,
     elf_fpregset_t,
@@ -17,6 +20,18 @@ from coredump_structs import (
 NT_PRXFPREG = 1189489535
 
 
+def page_start(v: int) -> int:
+    return v & ~(resource.getpagesize() - 1)
+
+
+def page_offset(v: int) -> int:
+    return v & (resource.getpagesize() - 1)
+
+
+def page_align(v: int) -> int:
+    return (v + resource.getpagesize() - 1) & ~(resource.getpagesize() - 1)
+
+
 class ElfCore:
     """
     Not a general purpose coredump parser, but specialized on what we generate int the
@@ -28,7 +43,22 @@ class ElfCore:
     special_regs: List["KVMSRegs"] = []
     msrs: List[List["kvm_msr_entry"]] = []
 
+    def map_segment(self, seg: Segment) -> bytes:
+        file_offset = seg.header.p_offset
+        file_size = seg.header.p_filesz
+        offset = page_start(file_offset)
+        delta = file_offset - offset
+        mm = mmap.mmap(
+            self.fd.fileno(),
+            length=seg.header.p_filesz + delta,
+            prot=mmap.PROT_READ,
+            offset=offset,
+        )
+        # remove page alignments needed for mmap again
+        return mm[(file_offset - offset) : (file_size - delta)]
+
     def __init__(self, fd: IO[bytes]) -> None:
+        self.fd = fd
         self.elf = ELFFile(fd)
         note_segment = next(self.elf.iter_segments())
         assert isinstance(note_segment, NoteSegment)
