337e367cd3e6be0fb5b09ec9d444b31482dce2c9
Pogobanane <aenderboy@gmx.de>
2021-04-05 10:31:26 +0000

    test insmod virtio_mmio: failing
    because the kernel is missing VIRTIO_MMIO_CMDLINE_DEVICES

--- tests/test_virtio_blk.py
+++ tests/test_virtio_blk.py
@@ -1,9 +1,9 @@
 import conftest
+from multiprocessing import Process
 
 
 def test_this_test(helpers: conftest.Helpers) -> None:
     with helpers.spawn_qemu(helpers.notos_image()) as vm:
-        print("wait for ssh")
         vm.wait_for_ssh()
         print("ssh available")
         res = vm.ssh_cmd_sh("insmod $MODULE_DRIVERS_DIR/virtio/virtio_mmio.ko.xz")
@@ -14,12 +14,25 @@ def test_this_test(helpers: conftest.Helpers) -> None:
         assert res.stdout.find("virtio_mmio") >= 0
 
 
-# def test_virtio_device_space(helpers: conftest.Helpers) -> None:
-#     with helpers.spawn_qemu(helpers.notos_image()) as vm:
-#         print("wait for ssh")
-#         vm.wait_for_ssh()
-#         print("ssh available")
-#         vm.attach()
-#         res = vm.ssh_cmd(["insmod", "virtio_mmio"])
-#         print("stdout:\n", res.stdout)
-#         print("stderr:\n", res.stderr)
+def test_virtio_device_space(helpers: conftest.Helpers) -> None:
+    with helpers.spawn_qemu(helpers.notos_image()) as vm:
+        vmsh = Process(target=helpers.run_vmsh_command, args=(["attach", str(vm.pid)],))
+        vmsh.start()
+        vm.wait_for_ssh()
+        print("ssh available")
+
+        mmio_config = "0x1000@0xd0000000:5"
+        res = vm.ssh_cmd_sh(
+            f"insmod $MODULE_DRIVERS_DIR/virtio/virtio_mmio.ko.xz device={mmio_config}"
+        )
+        print("stdout:\n", res.stdout)
+        print("stderr:\n", res.stderr)
+
+        res = vm.ssh_cmd(["dmesg"])
+        print("stdout:\n", res.stdout)
+
+        vmsh.kill()
+        vmsh.join(10)
+        vmsh.terminate()
+
+        assert res.stdout.find("virtio_mmio: unknown parameter 'device' ignored") < 0
