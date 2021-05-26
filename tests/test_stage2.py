dfe0de7856489f8648e9dc5544d61393cdc7ebd2
Jörg Thalheim <joerg@thalheim.io>
2021-05-26 17:01:34 +0200

    use busybox image in stage2 test

--- tests/test_stage2.py
+++ tests/test_stage2.py
@@ -7,18 +7,18 @@ def test_stage2(helpers: conftest.Helpers) -> None:
     proc = subprocess.run(["cargo", "build"], cwd=root.joinpath("..", "src", "stage2"))
     assert proc.returncode == 0
     # TODO
-    image = root.joinpath("..", "..", "linux", "nixos.ext4")
-    extra_args = [
-        "-drive",
-        f"index=1,id=drive2,file={image},format=raw,if=none",
-        "-device",
-        "virtio-blk-pci,drive=drive2,serial=vmsh0,bootindex=2,serial=vmsh0",
-    ]
+    with helpers.busybox_image() as image:
+        extra_args = [
+            "-drive",
+            f"index=1,id=drive2,file={image},format=raw,if=none",
+            "-device",
+            "virtio-blk-pci,drive=drive2,serial=vmsh0,bootindex=2,serial=vmsh0",
+        ]
 
-    with helpers.spawn_qemu(helpers.notos_image(), extra_args) as vm:
-        vm.wait_for_ssh()
-        # vm.attach()
-        res = vm.ssh_cmd(
-            ["/vmsh/src/stage2/target/debug/stage2"], check=False, stdout=None
-        )
-        assert res.returncode == 0
+        with helpers.spawn_qemu(helpers.notos_image(), extra_args) as vm:
+            vm.wait_for_ssh()
+            # vm.attach()
+            res = vm.ssh_cmd(
+                ["/vmsh/src/stage2/target/debug/stage2"], check=False, stdout=None
+            )
+            assert res.returncode == 0
