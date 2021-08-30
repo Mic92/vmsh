aa60e3eadba24dee06fd4c1bbdefa772a42f5503
Jörg Thalheim <joerg@thalheim.io>
2021-08-30 14:21:38 +0000

    add xfstest-user

--- nix/modules/measurement-config.nix
+++ nix/modules/measurement-config.nix
@@ -11,4 +11,16 @@ in {
     phoronix
     myxfstests
   ];
+  environment.etc.passwd.text = ''
+    daemon:1:daemon:/usr/sbin:/noshell
+    fsgqa:x:1001:1002:Fsgqa:/:/bin/sh
+    fsgqa2:x:1001:1002:Fsgqa:/:/bin/sh
+    123456-fsgqa:x:1003:1003:Fsgqa:/:/bin/sh
+  '';
+  group.text = ''
+    daemon:x:1:
+    fsgqa:x:1001:
+    fsgqa2:x:1002:
+    123456-fsgqa:x:1003:
+  '';
 }
