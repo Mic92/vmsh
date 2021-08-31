e5f10266afe6e6e148d44dd5e9d24c697a8abf3e
Pogobanane <aenderboy@gmx.de>
2021-08-31 13:25:58 +0000

    add xfsdump and fix more

--- nix/xfstests.nix
+++ nix/xfstests.nix
@@ -3,8 +3,11 @@
 , fetchgit, fio, gawk, keyutils, killall, lib, libaio, libcap, libtool
 , libuuid, libxfs, lvm2, openssl, perl, procps, quota
 , time, util-linux, which, writeScript, xfsprogs, runtimeShell, mktemp
-, hostname, gnused, diffutils, findutils, glibc }:
+, hostname, gnused, diffutils, findutils, glibc, callPackage }:
 
+let
+  xfsdump = callPackage ./xfsdump.nix { };
+in
 stdenv.mkDerivation {
   name = "xfstests-2021-08-22";
 
@@ -28,6 +31,12 @@ stdenv.mkDerivation {
     # Patch an incompatible awk script with sed. Genious.
     sed -i 's/\/\^\\#\//\/\^#\//' tests/generic/001
 
+    # fix using coreutils
+    sed -i 's/SCRATCH_MNT\/ls_on_scratch/SCRATCH_MNT\/ls/' tests/generic/452 tests/generic/452.out
+
+    # Fix shell-less fsgqa user
+    sed -i 's/su $qa_user/su -s \/bin\/sh $qa_user/' common/rc
+
     substituteInPlace Makefile \
       --replace "cp include/install-sh ." "cp -f include/install-sh ."
 
@@ -47,7 +56,7 @@ stdenv.mkDerivation {
     # Fix up lots of impure paths
     for f in common/* tools/* tests/*/*; do
       sed -i $f -e 's|/bin/bash|${bash}/bin/bash|'
-      sed -i $f -e 's|/bin/true|true|'
+      sed -i $f -e 's|/bin/true|${coreutils}/bin/true|'
       sed -i $f -e 's|/usr/sbin/filefrag|${e2fsprogs}/bin/filefrag|'
       sed -i $f -e 's|hostname -s|hostname|'   # `hostname -s` seems problematic on NixOS
       sed -i $f -e 's|$(_yp_active)|1|'        # NixOS won't ever have Yellow Pages enabled
@@ -112,6 +121,7 @@ stdenv.mkDerivation {
                                    which
                                    xfsprogs
                                    glibc # for getconf
+                                   xfsdump
                                    ])}:$PATH
     exec ./check "$@"
   '';
