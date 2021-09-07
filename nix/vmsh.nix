7bc9de1583ee1da0efafa634f1cc4b17d410e60b
Pogobanane <aenderboy@gmx.de>
2021-09-07 15:57:29 +0000

    vm-memory: bump version
    fixes aligned loads/stores

--- nix/vmsh.nix
+++ nix/vmsh.nix
@@ -14,7 +14,7 @@ rustPlatform.buildRustPackage {
       "bcc-0.0.32-alpha.0" = "sha256-BEegNjqf+6xi/xchyl0Oaf5/pHlC4+/iaoZPsS0Va2g=";
       "virtio-blk-0.1.0" = "sha256-3eXSPy3+5uI0FBpSVwRKWJmWxgrpwfl4rYpPLn0bf/4=";
       "vm-device-0.1.0" = "sha256-kHiEfk3/+ped39Dm4Lzo62E7IWiVDd+PnSsPr1YDj94=";
-      "vm-memory-0.5.0" = "sha256-vnSWs9n5nRDO0R8eKII/7P5c731cVN02iQh3L7AdoQA=";
+      "vm-memory-0.5.0" = "sha256-7Ii+GP3vWhTT6dJLCLw7F3hjuvd8F/jNxEDDTMzLXIM=";
     };
   };
 }
