3edcb4f1af84724fbef16bb3225ff1d90822c991
Jörg Thalheim <joerg@thalheim.io>
2023-06-05 06:57:46 +0200

    switch from flake-utils to flake-parts

--- flake.nix
+++ flake.nix
@@ -4,6 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
     not-os.url = "github:cleverca22/not-os";
     not-os.flake = false;
     flake-utils.url = "github:numtide/flake-utils";
@@ -16,184 +19,166 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix, not-os, microvm }:
-    (flake-utils.lib.eachSystem ["x86_64-linux"] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        fenixPkgs = fenix.packages.${system};
-        rustToolchain = with fenixPkgs; combine [
-          stable.cargo
-          stable.rustc
-          stable.rust-src
-          stable.rust-std
-          stable.clippy
-          stable.rustfmt
-          targets.x86_64-unknown-linux-musl.stable.rust-std
-          # fenix.packages.x86_64-linux.targets.aarch64-unknown-linux-gnu.latest.rust-std
-        ];
-
-        rustPlatform = (pkgs.makeRustPlatform {
-          cargo = rustToolchain;
-          rustc = rustToolchain;
-        });
-
-        vmsh = pkgs.callPackage ./nix/vmsh.nix {
-          pkgSrc = self;
-          inherit rustPlatform;
-        };
-
-        kernel-deps = pkgs.callPackage ./nix/kernel-deps.nix {};
-        kernel-deps-no-fhs = pkgs.callPackage ./nix/kernel-deps-no-fhs.nix {};
-        kernel-deps-shell = (pkgs.callPackage ./nix/kernel-deps.nix {
-          runScript = "bash";
-        });
-
-
-        measureDeps = [
-          # only used in tests/reproduce.py
-          pkgs.lsof
-
-          pkgs.numactl
-          pkgs.fio
-          (pkgs.python3.withPackages (ps: [
-            ps.seaborn
-            ps.pandas
-            ps.psutil
-            ps.natsort
-          ]))
-        ];
-
-        ciDeps = [
-          rustToolchain
-          pkgs.qemu_kvm
-          pkgs.tmux # needed for integration test
-          (pkgs.python3.withPackages (ps: [
-            ps.pytest
-            ps.pytest-xdist
-            ps.pyelftools
-            ps.intervaltree
-            ps.lxml
-
-            # linting
-            ps.black
-            ps.flake8
-            ps.isort
-            ps.mypy
-          ]))
-        ] ++ vmsh.nativeBuildInputs ++ measureDeps;
-
-        not-os-image' = (pkgs.callPackage ./nix/not-os-image.nix {
-          inherit not-os;
-          notos-config = [
-            ./nix/modules/not-os-config.nix
+  outputs = inputs @ { flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ self, lib, ... }: {
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, pkgs, inputs', ... }:
+        let
+          fenixPkgs = inputs'.fenix.packages;
+          rustToolchain = with fenixPkgs; combine [
+            stable.cargo
+            stable.rustc
+            stable.rust-src
+            stable.rust-std
+            stable.clippy
+            stable.rustfmt
+            targets.x86_64-unknown-linux-musl.stable.rust-std
+            # fenix.packages.x86_64-linux.targets.aarch64-unknown-linux-gnu.latest.rust-std
           ];
-        });
-        not-os-image_4_19 = (not-os-image'.override { linuxPackages = pkgs.linuxPackages_4_19; }).json;
-        not-os-image_5_10 = (not-os-image'.override { linuxPackages = pkgs.linuxPackages_5_10; }).json;
-        not-os-image_5_15 = (not-os-image'.override { linuxPackages = pkgs.linuxPackages_5_15; }).json;
-        not-os-image_6_1 = (not-os-image'.override { linuxPackages = pkgs.linuxPackages_6_1; }).json;
+          rustPlatform = (pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          });
 
-        measurement-image = (pkgs.callPackage ./nix/not-os-image.nix {
-          inherit not-os;
-          notos-config = [
-            ./nix/modules/measurement-config.nix
-            # we mainly need this for XFS_ONLINE_SCRUB
-            self.nixosModules.linux-ioregionfd
+          measureDeps = [
+            # only used in tests/reproduce.py
+            pkgs.lsof
+
+            pkgs.numactl
+            pkgs.fio
+            (pkgs.python3.withPackages (ps: [
+              ps.seaborn
+              ps.pandas
+              ps.psutil
+              ps.natsort
+            ]))
           ];
-        }).json;
 
-        linux_ioregionfd = pkgs.callPackage ./nix/linux-ioregionfd.nix { };
-        kvmtool = pkgs.callPackage ./nix/kvmtool.nix { };
-        xfstests = pkgs.callPackage ./nix/xfstests.nix { };
-      in {
-        # default target for `nix build`
-        defaultPackage = vmsh;
-        packages = rec {
-          inherit vmsh linux_ioregionfd;
 
-          # see justfile/build-linux-shell
-          inherit kernel-deps;
+          ciDeps = [
+            rustToolchain
+            pkgs.qemu_kvm
+            pkgs.tmux # needed for integration test
+            (pkgs.python3.withPackages (ps: [
+              ps.pytest
+              ps.pytest-xdist
+              ps.pyelftools
+              ps.intervaltree
+              ps.lxml
 
-          # for testing
-          inherit kvmtool;
+              # linting
+              ps.black
+              ps.flake8
+              ps.isort
+              ps.mypy
+            ]))
+          ] ++ config.packages.vmsh.nativeBuildInputs ++ measureDeps;
 
-          # used in tests/xfstests.py
-          inherit xfstests;
+          not-os-image' = (pkgs.callPackage ./nix/not-os-image.nix {
+            inherit (inputs) not-os;
+            notos-config = [
+              ./nix/modules/not-os-config.nix
+            ];
+          });
+        in
+        {
+          # default target for `nix build`
+          packages = {
+            vmsh = pkgs.callPackage ./nix/vmsh.nix {
+              pkgSrc = self;
+              inherit rustPlatform;
+            };
+            linux_ioregionfd = pkgs.callPackage ./nix/linux-ioregionfd.nix { };
+            default = config.packages.vmsh;
+            # see justfile/build-linux-shell
+            kernel-deps = pkgs.callPackage ./nix/kernel-deps.nix { };
 
-          # see justfile/not-os
-          not-os-image = not-os-image'.json;
-          inherit
-            not-os-image_4_19
-            not-os-image_5_10
-            not-os-image_5_15
-            not-os-image_6_1;
-          inherit measurement-image;
+            # for testing
+            kvmtool = pkgs.callPackage ./nix/kvmtool.nix { };
+            # used in tests/xfstests.py
+            xfstests = pkgs.callPackage ./nix/xfstests.nix { };
 
-          # see justfile/nixos-image
-          nixos-image = pkgs.callPackage ./nix/nixos-image.nix {};
-          busybox-image = pkgs.callPackage ./nix/busybox-image.nix {};
-          passwd-image = pkgs.callPackage ./nix/passwd-image.nix {};
-          alpine-sec-scanner = pkgs.callPackage ./nix/alpine-sec-scanner.nix {};
-          alpine-db = pkgs.callPackage ./nix/alpine-db.nix {};
-          alpine-sec-scanner-image = pkgs.callPackage ./nix/alpine-sec-scanner-image.nix {};
-          phoronix-image = pkgs.callPackage ./nix/phoronix-image.nix {};
-          alpine-image = pkgs.callPackage ./nix/alpine-image.nix {};
-          fat-image = pkgs.callPackage ./nix/fat-image.nix {};
+            not-os-image_4_19 = (not-os-image'.override { linuxPackages = pkgs.linuxPackages_4_19; }).json;
+            not-os-image_5_10 = (not-os-image'.override { linuxPackages = pkgs.linuxPackages_5_10; }).json;
+            not-os-image_5_15 = (not-os-image'.override { linuxPackages = pkgs.linuxPackages_5_15; }).json;
+            not-os-image_6_1 = (not-os-image'.override { linuxPackages = pkgs.linuxPackages_6_1; }).json;
 
-          inherit (microvm.packages.${pkgs.system})
-            firecracker-example crosvm-example kvmtool-example qemu-example;
+            measurement-image = (pkgs.callPackage ./nix/not-os-image.nix {
+              inherit (inputs) not-os;
+              notos-config = [
+                ./nix/modules/measurement-config.nix
+                # we mainly need this for XFS_ONLINE_SCRUB
+                self.nixosModules.linux-ioregionfd
+              ];
+            }).json;
 
-          phoronix-test-suite = pkgs.callPackage ./nix/phoronix.nix {};
-        };
-        devShells  = rec {
-          # used in .drone.yml
-          ci-shell = pkgs.mkShell {
-            inherit (vmsh) buildInputs;
-            nativeBuildInputs = ciDeps;
+            inherit (inputs'.microvm.packages)
+              firecracker-example crosvm-example kvmtool-example qemu-example;
+
+            # see justfile/nixos-image
+            nixos-image = pkgs.callPackage ./nix/nixos-image.nix { };
+            busybox-image = pkgs.callPackage ./nix/busybox-image.nix { };
+            passwd-image = pkgs.callPackage ./nix/passwd-image.nix { };
+            alpine-sec-scanner = pkgs.callPackage ./nix/alpine-sec-scanner.nix { };
+            alpine-db = pkgs.callPackage ./nix/alpine-db.nix { };
+            alpine-sec-scanner-image = pkgs.callPackage ./nix/alpine-sec-scanner-image.nix { };
+            phoronix-image = pkgs.callPackage ./nix/phoronix-image.nix { };
+            alpine-image = pkgs.callPackage ./nix/alpine-image.nix { };
+            fat-image = pkgs.callPackage ./nix/fat-image.nix { };
+
+            phoronix-test-suite = pkgs.callPackage ./nix/phoronix.nix {};
           };
-          # see justfile/build-linux-shell
-          inherit kernel-deps-shell;
-          inherit kernel-deps-no-fhs;
-        };
-        # not supported by nix flakes yet, but useful
-        packageSets = rec {
-          linuxPackages_ioregionfd = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_ioregionfd);
-        };
-        checks = self.packages.${system};
-        # used by `nix develop`
-        devShells.default = pkgs.mkShell {
-          inherit (vmsh) buildInputs;
-          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
-          nativeBuildInputs = ciDeps ++ [
-            pkgs.socat
-            pkgs.jq # needed for justfile
-            pkgs.just
-            pkgs.cargo-watch
-            pkgs.cargo-deny
-            pkgs.rust-analyzer
-            pkgs.gdb
-            # pkgs.libguestfs-with-appliance # needed for just attach-qemu-img and thus stress-test
-            pkgs.gnuplot
-            pkgs.cloud-hypervisor
-            pkgs.crosvm
-            pkgs.firectl
-            kvmtool
-            pkgs.firecracker
+          legacyPackages = {
+            linuxPackages_ioregionfd = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor config.packages.linux_ioregionfd);
+          };
+          # used by `nix develop`
 
-            # for xfstests:
-            pkgs.parted
-            pkgs.xfsprogs
-            xfstests
-          ];
+          devShells = {
+            default = pkgs.mkShell {
+              inherit (config.packages.vmsh) buildInputs;
+              RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+              nativeBuildInputs = ciDeps ++ [
+                pkgs.socat
+                pkgs.jq # needed for justfile
+                pkgs.just
+                pkgs.cargo-watch
+                pkgs.cargo-deny
+                pkgs.rust-analyzer
+                pkgs.gdb
+                # pkgs.libguestfs-with-appliance # needed for just attach-qemu-img and thus stress-test
+                pkgs.gnuplot
+                pkgs.cloud-hypervisor
+                pkgs.crosvm
+                pkgs.firectl
+                config.packages.kvmtool
+                pkgs.firecracker
 
-          # interesting when supporting aarch64
-          #CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
-          #  "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/aarch64-unknown-linux-gnu-gcc";
+                # for xfstests:
+                pkgs.parted
+                pkgs.xfsprogs
+                config.packages.xfstests
+              ];
+
+              # interesting when supporting aarch64
+              #CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
+              #  "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/aarch64-unknown-linux-gnu-gcc";
+            };
+            kernel-deps-no-fhs = pkgs.callPackage ./nix/kernel-deps-no-fhs.nix { };
+            kernel-deps-shell = (pkgs.callPackage ./nix/kernel-deps.nix {
+              runScript = "bash";
+            });
+            # used in .drone.yml
+            ci-shell = pkgs.mkShell {
+              inherit (config.packages.vmsh) buildInputs;
+              nativeBuildInputs = ciDeps;
+            };
+          };
         };
-      })) // {
+      flake = {
         nixosModules.linux-ioregionfd = { pkgs, lib, ... }: {
           boot.kernelPackages = lib.mkForce self.packageSets.${pkgs.system}.linuxPackages_ioregionfd;
         };
-        lib.nixpkgsRev = nixpkgs.shortRev;
+        lib.nixpkgsRev = inputs.nixpkgs.shortRev;
       };
+    });
 }
